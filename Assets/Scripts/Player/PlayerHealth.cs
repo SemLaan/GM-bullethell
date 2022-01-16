c584c872833dedd9c3574eb07d15abef06f56740
sem laan <semlaan@dds.nl>
2022-01-16 21:36:15 +0100

    There is now a ui element that shows the players health

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private int health;
+    [SerializeField] private Text healthDisplay;
 
     public void removeHealth(int amount)
     {
         health -= amount;
+        healthDisplay.text = "Health: " + health;
         if (health <= 0)
         {
             gameOver();
