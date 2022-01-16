0a8d79586f1a0f480d5a2a0b1be025496e26293a
sem laan <semlaan@dds.nl>
2022-01-16 15:35:00 +0100

    Made a player health script that keeps track of the players health and calls a game over function if health goes below 1

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private int health;
+
+    public void removeHealth(int amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            gameOver();
+        }
+    }
+
+    private void gameOver()
+    {
+
+    }
+}
