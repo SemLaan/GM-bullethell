3afd70ad1baaebda1da2abb1d598234e0b7ddcc2
sem laan <semlaan@dds.nl>
2022-01-20 11:25:58 +0100

    Bullets rows can now be created at the bottom and can reach the top right corner

--- Assets/Scripts/Bullets/RowPattern.cs
+++ Assets/Scripts/Bullets/RowPattern.cs
@@ -6,16 +6,16 @@ public class RowPattern : IPattern
 {
     public void CreateBulletPattern(int bullets, GameObject bulletPrefab, TurretManager turretManager, MapController mapController)
     {
-        Direction wall = (Direction)Random.Range(0, 3);
+        Direction wall = (Direction)Random.Range(0, 4);
         int startOfRow;
         if (wall == Direction.up || wall == Direction.down) // if wall is up or down
         {
             int wallLength = mapController.mapSizeInTiles.x;
-            startOfRow = Random.Range(0, wallLength - bullets);
+            startOfRow = Random.Range(0, wallLength - bullets + 1);
         } else //if wall is left or right
         {
             int wallLength = mapController.mapSizeInTiles.y;
-            startOfRow = Random.Range(0, wallLength - bullets);
+            startOfRow = Random.Range(0, wallLength - bullets + 1);
         }
         for (int i = startOfRow; i < startOfRow + bullets; i++)
         {
